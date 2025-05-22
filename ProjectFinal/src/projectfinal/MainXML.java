46b6cba3ea43f1cc2f78088735f3845d0760a5d9
Sergio7DAW <sergio.daw9@gmail.com>
2025-05-22 13:13:38 +0200

    5 commit

---
+++ ProjectFinal/src/projectfinal/MainXML.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectfinal;
+
+/**
+ *
+ * @author 1DAW2425-09
+ */
+import java.util.List;
+
+public class MainXML {
+
+    public static void main(String[] args) {
+        GestionUsuarios gestion = new GestionUsuarios();
+        XMLManager xmlManager = new XMLManager();
+
+        // Insertar usuarios manualmente
+        gestion.insertarUsuario(new Usuario("Ana", "abc123hashed"));
+        gestion.insertarUsuario(new Usuario("Luis", "pass456hashed"));
+
+        // Guardar a XML
+        String path = "C:\\Users\\1DAW2425-09\\Downloads\\usuarios.xml";
+        xmlManager.guardarUsuariosEnXML(gestion.usuariosLista, path);
+        //xmlManager.guardarUsuariosEnXML(gestion.usuariosLista, "usuarios.xml");
+
+        // Cargar desde XML
+        List<Usuario> cargados = xmlManager.cargarUsuariosDesdeXML("C:\\Users\\1DAW2425-09\\Downloads\\usuarios.xml");
+        //List<Usuario> cargados = xmlManager.cargarUsuariosDesdeXML("usuarios.xml");
+
+        // Mostrar usuarios cargados
+        System.out.println("Usuarios cargados desde XML:");
+        for (Usuario u : cargados) {
+            System.out.println(u.listarUsuario());
+        }
+    }
+}
